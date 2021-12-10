1ebe4006b4ef0fe699031cf2138f4d8efe852bbc
Namae Conde <lunamae.conde@sensis.com.au>
2021-12-11 00:39:29 +1100

    Update navbar message for logged in user

--- src/components/dashboard-navbar.js
+++ src/components/dashboard-navbar.js
@@ -1,11 +1,10 @@
 import PropTypes from 'prop-types';
 import styled from '@emotion/styled';
-import { AppBar, Avatar, Badge, Box, IconButton, Toolbar, Tooltip } from '@mui/material';
+import { AppBar, Avatar, Badge, Box, IconButton, Toolbar, Tooltip, Typography } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
 import { Bell as BellIcon } from '../icons/bell';
 import { UserCircle as UserCircleIcon } from '../icons/user-circle';
-import { Users as UsersIcon } from '../icons/users';
 
 const DashboardNavbarRoot = styled(AppBar)(({ theme }) => ({
   backgroundColor: theme.palette.background.paper,
@@ -52,11 +51,12 @@ export const DashboardNavbar = (props) => {
             </IconButton>
           </Tooltip>
           <Box sx={{ flexGrow: 1 }} />
-          <Tooltip title="Contacts">
-            <IconButton sx={{ ml: 1 }}>
-              <UsersIcon fontSize="small" />
-            </IconButton>
-          </Tooltip>
+          <Typography
+            color="textSecondary"
+            variant="subtitle1"
+          >
+            Welcome back, Mayor!
+          </Typography>
           <Tooltip title="Notifications">
             <IconButton sx={{ ml: 1 }}>
               <Badge
