ee0982baa44a91b8a8b759eedd03102480ae9e78
Namae Conde <lunamae.conde@sensis.com.au>
2021-12-11 00:11:56 +1100

    Add main dashboard layout

---
+++ src/pages/index.js
@@ -0,0 +1,30 @@
+import Head from 'next/head';
+import { Box } from '@mui/material';
+import { DashboardLayout } from '../components/dashboard-layout';
+
+const Dashboard = () => (
+  <>
+    <Head>
+      <title>
+        Dashboard
+      </title>
+    </Head>
+    <Box
+      component="main"
+      sx={{
+        flexGrow: 1,
+        py: 8
+      }}
+    >
+      Main
+    </Box>
+  </>
+);
+
+Dashboard.getLayout = (page) => (
+  <DashboardLayout>
+    {page}
+  </DashboardLayout>
+);
+
+export default Dashboard;
